a06245c94de003e891bfb155af17d9a64bf3d96d
Rpratik13 <r.pratik013@gmail.com>
2020-08-17 22:32:30 +0545

    Add build folder

---
+++ build/precache-manifest.1abb5822775c8cc2b70623cfb7b44a3f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "42479140dad564a3388c60f7d6a2e58a",
+    "url": "./api/register/index.html"
+  },
+  {
+    "revision": "30b467f4b499b925d285",
+    "url": "./api/register/static/css/main.aa8aa891.chunk.css"
+  },
+  {
+    "revision": "5540a42a87229fe6eb4c",
+    "url": "./api/register/static/js/2.afcdfd4f.chunk.js"
+  },
+  {
+    "revision": "ef7f0128066dad638e14e7838f835a83",
+    "url": "./api/register/static/js/2.afcdfd4f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "30b467f4b499b925d285",
+    "url": "./api/register/static/js/main.43484d66.chunk.js"
+  },
+  {
+    "revision": "74b10c3bf93bba3a1bd5",
+    "url": "./api/register/static/js/runtime-main.5d413c23.js"
+  }
+]);
